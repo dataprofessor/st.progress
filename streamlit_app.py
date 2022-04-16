80a7118515fbf7198ab89c63dbc0f570398fea75
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-16 21:49:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,4 @@ my_bar = st.progress(0)
 for percent_complete in range(100):
      time.sleep(0.1)
      my_bar.progress(percent_complete + 1)
-     st.write(percent_complete)
-     st.balloons()
+st.balloons()
